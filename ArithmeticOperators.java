ed8ac67483bca717e6eb763278ed747b4155066e
preethiramaiya <113494119+preethiramaiya@users.noreply.github.com>
2022-09-17 20:10:15 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package arithmetic;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		int a=30, b=50;
+		System.out.println("Subtraction of 2 numbers ="+(a-b));
+		System.out.println("Multiplication of 2 numbers ="+(a*b));
+		System.out.println("Division of 2 numbers ="+(a/b));
+		System.out.println("Addition of 2 numbers ="+(a+b));
+
+	}
+
+}
