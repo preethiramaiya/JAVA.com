5c395a83881c8a1facf7e076f252c7280962a3af
preethiramaiya <113494119+preethiramaiya@users.noreply.github.com>
2022-09-17 20:08:51 +0530

    Add files via upload

---
+++ AreaJava.java
@@ -0,0 +1,22 @@
+package com.area;
+
+public class AreaJava {
+
+	public static void main(String[] args) {
+		int a=2,b=4,h=7,r=5, Square, Triangle, Rectangle;
+		double Circle;
+		Square = a*a;
+		Triangle = (b*h)/2;
+		Rectangle = a*b;
+		Circle = r*r*3.14;
+		System.out.println("Area of Square ="+Square);
+		System.out.println("Area of Triangle ="+Triangle);
+		System.out.println("Area of Rectangle ="+Rectangle);
+		System.out.println("Area of Circle ="+Circle);
+		
+		
+		
+
+	}
+
+}
