3ffe65d0526e55c7e78038dd5d3c9687c180a505
preethiramaiya <113494119+preethiramaiya@users.noreply.github.com>
2022-09-22 20:54:53 +0530

    Add files via upload

---
+++ StudentAge.java
@@ -0,0 +1,21 @@
+package com.stud.array;
+
+public class StudentAge {
+	
+	public void Studentage() {
+		//local variable
+		int age=0;
+		age=age+22;
+		
+		System.out.println("Student Age is:"+age);
+		
+
+	}
+
+public static void main(String[] args) {
+	//local variable
+	StudentAge temp= new StudentAge();
+	temp.Studentage();
+}
+
+}
