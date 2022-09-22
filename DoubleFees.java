e441fd73dc552646950e0d0d98bbd0cbe2e234fd
preethiramaiya <113494119+preethiramaiya@users.noreply.github.com>
2022-09-22 20:53:05 +0530

    Add files via upload

---
+++ DoubleFees.java
@@ -0,0 +1,20 @@
+package com.stud.array;
+
+
+class student {
+	//static variable
+	public static double fees;
+	public static String name ="Preethi";
+}
+
+	
+public class DoubleFees {
+
+	public static void main(String[] args) {
+		    int student_fees=55775;
+		System.out.println("Preethi fees is:"+student_fees);		
+		
+
+	}
+
+}
